c877406ad1e81df049a74874be136b638e1cada2
jaineel-soni <140881565+jaineel-soni@users.noreply.github.com>
2023-07-30 12:41:04 +0530

    Created Index.php for calculator

---
+++ Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="functions.php" method="get">
+        <input type="text" name="num01" placeholder="number 1">
+        <select name="oper">
+            <label>Choose the operation</label>
+            <option value="sub">SUB</option>
+            <option value="add">ADD</option>
+        </select>
+        <input type="text" name="num02" placeholder="number 2">
+        <button type="submit">calculate!</button>
+    </form>
+</body>
+</html>
